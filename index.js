51d5ddae978ca9187eef46ff0a4b811d035e12c9
PJxDev <pedrocruces@hotmail.es>
2022-09-10 00:10:47 +0200

    [GAME] -- game on !

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const $ = (el) => document.querySelector(el);
 const $$ = (el) => document.querySelectorAll(el);
 
-$("h1").innerHTML = "Hola Mundo";
+// $("h1").innerHTML = "Hola Mundo";
 
 const canvas = $("canvas");
 const c = canvas.getContext("2d");
@@ -20,6 +20,8 @@ class Player {
     this.velocity = velocity;
     this.size = size;
     this.lastKey = "";
+    this.direction = { x: 1, y: 1 };
+    this.score = 0;
   }
   draw() {
     c.fillStyle = "white";
@@ -33,21 +35,8 @@ class Player {
 
   update() {
     this.draw();
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
-
-    if (
-      this.position.y + this.size.height + this.velocity.y >= cH ||
-      this.position.y <= 0
-    ) {
-      this.velocity.y *= -1;
-    }
-    if (
-      this.position.x + this.size.width + this.velocity.x >= cW ||
-      this.position.x <= 0
-    ) {
-      this.velocity.x *= -1;
-    }
+    this.position.y += this.velocity.y * this.direction.y;
+    this.position.x += this.velocity.x * this.direction.x;
   }
 }
 
@@ -59,7 +48,7 @@ const player1 = new Player({
   },
   velocity: {
     x: 0,
-    y: 0,
+    y: 3,
   },
   size: {
     width: 30,
@@ -69,32 +58,38 @@ const player1 = new Player({
 
 const player2 = new Player({
   position: {
-    x: cW - 50,
+    x: cW - 80,
     y: cH / 2 - 50,
   },
   velocity: {
     x: 0,
-    y: 0,
+    y: 3,
   },
   size: {
     width: 30,
     height: 100,
   },
 });
-const bola = new Player({
+const ball = new Player({
   position: {
     x: cW / 2 - 7,
     y: cH / 2 - 7,
   },
   velocity: {
-    x: 3,
-    y: 3,
+    x: 4,
+    y: 4,
   },
   size: {
     width: 15,
     height: 15,
   },
 });
+
+$("#score-p1").innerHTML = player1.score;
+$("#score-p2").innerHTML = player2.score;
+
+let pause = false;
+let oldVelocity = { x: 0, y: 0 };
 
 const keys = {
   w: {
@@ -103,10 +98,10 @@ const keys = {
   s: {
     pressed: false,
   },
-  n8: {
+  up: {
     pressed: false,
   },
-  n2: {
+  down: {
     pressed: false,
   },
 };
@@ -118,77 +113,140 @@ function animate() {
   c.fillRect(0, 0, cW, cH);
   player1.update();
   player2.update();
-  bola.update();
+  ball.update();
 
-  player1.velocity.y = 0;
-  player2.velocity.y = 0;
+  player1.direction.y = 0;
+  player2.direction.y = 0;
 
   /* PLAYERS MOVEMENT */
   if (keys.w.pressed && player1.lastKey === "w") {
-    player1.velocity.y = -2;
+    player1.direction.y = -1;
   } else if (keys.s.pressed && player1.lastKey === "s") {
-    player1.velocity.y = 2;
+    player1.direction.y = 1;
   }
-  if (keys.n8.pressed && player2.lastKey === "8") {
-    player2.velocity.y = -2;
-  } else if (keys.n2.pressed && player2.lastKey === "2") {
-    player2.velocity.y = 2;
+  if (keys.up.pressed && player2.lastKey === "ArrowUp") {
+    player2.direction.y = -1;
+  } else if (keys.down.pressed && player2.lastKey === "ArrowDown") {
+    player2.direction.y = 1;
   }
 
   /* COLLISIONS */
 
-  /* player 2 - frontal */
+  /* ball */
   if (
-    bola.position.x + bola.size.width >= player2.position.x &&
-    (bola.position.y >= player2.position.y &&
-    bola.position.y <= player2.position.y + player2.size.height)
+    ball.position.y + ball.size.height + ball.velocity.y >= cH ||
+    ball.position.y <= 0
   ) {
-    bola.velocity.x *= -1;
+    ball.velocity.y *= -1;
   }
-  /* player 2 - trasera */
   if (
-    bola.position.x >= player2.position.x + player2.size.width &&
-    (bola.position.y >= player2.position.y &&
-    bola.position.y <= player2.position.y + player2.size.height)
+    ball.position.x + ball.size.width + ball.velocity.x >= cW ||
+    ball.position.x + ball.velocity.x <= 0
   ) {
-    bola.velocity.x *= -1;
+    ball.velocity.x = 0;
+    ball.velocity.y = 0;
+    ballOut();
   }
 
-  /* player 1 - frontal */
-  if (
-    bola.position.x <= player1.position.x + player1.size.width &&
-    (bola.position.y >= player1.position.y &&
-      bola.position.y <= player1.position.y + player1.size.height)
-      ) {
-        bola.velocity.x *= -1;
-      }
-  /* player 1 - trasera */
-  if (
-    bola.position.x >= player1.position.x &&
-    (bola.position.y >= player1.position.y &&
-      bola.position.y <= player1.position.y + player1.size.height)
-      ) {
-        bola.velocity.x *= -1;
-      }
-  /* player 1 - lateral inferior */
-  if (
-    (bola.position.x <= player1.position.x + player1.size.width && 
-      bola.position.x >= player1.position.x) &&
-    (bola.position.y <= player1.position.y)
-  ) {
-    bola.velocity.y *= -1;
-    bola.velocity.x *= -1;
+  /* Players with the frame */
+
+  if (player1.position.y + player1.size.height >= cH) {
+    player1.position.y = cH - player1.size.height;
   }
-  /* player 1 - lateral superior */
-  if (
-    (bola.position.x <= player1.position.x + player1.size.width && 
-      bola.position.x >= player1.position.x) &&
-    (bola.position.y >= player1.position.y + player1.size.height)
-  ) {
-    bola.velocity.y *= -1;
-    bola.velocity.x *= -1;
+  if (player1.position.y <= 0) {
+    player1.position.y = 0;
   }
-  
+  if (player2.position.y + player2.size.height >= cH) {
+    player2.position.y = cH - player2.size.height;
+  }
+  if (player2.position.y <= 0) {
+    player2.position.y = 0;
+  }
+
+  /* Players with the ball */
+
+  /* Player2 */
+
+  if (
+    (ball.position.x + ball.size.width >= player2.position.x &&
+      ball.position.x <= player2.position.x &&
+      ball.position.y <= player2.position.y + player2.size.height &&
+      ball.position.y + ball.size.height >= player2.position.y) ||
+    (ball.position.x + ball.size.width >=
+      player2.position.x + player2.size.width &&
+      ball.position.x <= player2.position.x + player2.size.width &&
+      ball.position.y <= player2.position.y + player2.size.height &&
+      ball.position.y + ball.size.height >= player2.position.y)
+  ) {
+    ball.velocity.x *= -1;
+  }
+  if (
+    (ball.position.x + ball.size.width >= player2.position.x &&
+      ball.position.x <= player2.position.x + player2.size.width &&
+      ball.position.y + ball.size.height >= player2.position.y &&
+      ball.position.y <= player2.position.y) ||
+    (ball.position.x + ball.size.width >= player2.position.x &&
+      ball.position.x <= player2.position.x + player2.size.width &&
+      ball.position.y <= player2.position.y + player2.size.height &&
+      ball.position.y + ball.size.height >=
+        player2.position.y + player2.size.height)
+  ) {
+    ball.velocity.y *= -1;
+  }
+  /* Player1 */
+
+  if (
+    (ball.position.x + ball.size.width >= player1.position.x &&
+      ball.position.x <= player1.position.x &&
+      ball.position.y <= player1.position.y + player1.size.height &&
+      ball.position.y + ball.size.height >= player1.position.y) ||
+    (ball.position.x + ball.size.width >=
+      player1.position.x + player1.size.width &&
+      ball.position.x <= player1.position.x + player1.size.width &&
+      ball.position.y <= player1.position.y + player1.size.height &&
+      ball.position.y + ball.size.height >= player1.position.y)
+  ) {
+    ball.velocity.x *= -1;
+  }
+  if (
+    (ball.position.x + ball.size.width >= player1.position.x &&
+      ball.position.x <= player1.position.x + player1.size.width &&
+      ball.position.y + ball.size.height >= player1.position.y &&
+      ball.position.y <= player1.position.y) ||
+    (ball.position.x + ball.size.width >= player1.position.x &&
+      ball.position.x <= player1.position.x + player1.size.width &&
+      ball.position.y <= player1.position.y + player1.size.height &&
+      ball.position.y + ball.size.height >=
+        player1.position.y + player1.size.height)
+  ) {
+    ball.velocity.y *= -1;
+  }
+
+  /* BALL OUT */
+  function ballOut() {
+    if (ball.position.x > 50) {
+      $("#score-p1").innerHTML = ++player1.score;
+      setTimeout(ballInit, 2000, -1);
+    }
+    if (ball.position.x < 50) {
+      $("#score-p2").innerHTML = ++player2.score;
+      setTimeout(ballInit, 2000, 1);
+    }
+  }
+
+  function ballInit(n) {
+    let random = Math.floor(Math.random() * 2);
+    if (random != 1) random = -1;
+
+    ball.position.x = cW / 2 - 7;
+    ball.position.y = cH / 2 - 7;
+    ball.velocity.x = 4 * n;
+    ball.velocity.y = 4 * random;
+    player1.position.x = 50;
+    player1.position.y = cH / 2 - 50;
+    player2.position.x = cW - 80;
+    player2.position.y = cH / 2 - 50;
+  }
 }
 
 animate();
@@ -196,33 +254,48 @@ animate();
 /* EVENTS */
 
 window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "w":
-      keys.w.pressed = true;
-      player1.lastKey = "w";
-      break;
-    case "s":
-      keys.s.pressed = true;
-      player1.lastKey = "s";
-      break;
-    case "8":
-      keys.n8.pressed = true;
-      player2.lastKey = "8";
-      break;
-    case "2":
-      keys.n2.pressed = true;
-      player2.lastKey = "2";
-      break;
+  if (e.key === " ") {
+    pause = pause ? false : true;
+    console.log(pause);
+    if (pause) {
+      oldVelocity.x = ball.velocity.x;
+      ball.velocity.x = 0;
+      oldVelocity.y = ball.velocity.y;
+      ball.velocity.y = 0;
+    } else {
+      ball.velocity.x = oldVelocity.x;
+      ball.velocity.y = oldVelocity.y;
+    } 
+  }
+  if (!pause) {
+    switch (e.key) {
+      case "w":
+        keys.w.pressed = true;
+        player1.lastKey = "w";
+        break;
+      case "s":
+        keys.s.pressed = true;
+        player1.lastKey = "s";
+        break;
+      case "ArrowUp":
+        keys.up.pressed = true;
+        player2.lastKey = "ArrowUp";
+        break;
+      case "ArrowDown":
+        keys.down.pressed = true;
+        player2.lastKey = "ArrowDown";
+        break;
+    }
   }
 });
 
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
-    case "8":
-      keys.n8.pressed = false;
+    case "ArrowUp":
+      keys.up.pressed = false;
       break;
-    case "2":
-      keys.n2.pressed = false;
+    case "ArrowDown":
+      keys.down.pressed = false;
       break;
     case "w":
       keys.w.pressed = false;
